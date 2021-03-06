638897cce82f1c11ff9981db426121390b20ea5e
Immanuel Ben Hefer <immanuel.ben.hefer@sap.com>
2021-03-06 18:57:51 +0200

    remove setup file

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/elastic/elasticsearch/v$1/buildSrc/version.properties -O gradle.properties
-
-https://github.com/elastic/elasticsearch/blob/5.3/buildSrc/version.properties
